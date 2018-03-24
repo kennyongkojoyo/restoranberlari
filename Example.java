2b36f704888d03bbe65fb1672d47eeff448020fc
ambarsetiaawan <ambarsetiaawan@gmail.com>
2018-03-24 19:14:39 +0700

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,17 @@
+//package pathfinding;
+
+//import pathfinding.Grid2d;
+
+public class Example {
+	public Example() {
+		//double[][] map = { { 0, 1, 2 }, { 3, 3, 2 }, { 0, -1, 0 } };
+		double[][] map = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+		Grid2d map2d = new Grid2d(map, false);
+		System.out.println(map2d.findPath(0, 0, 2, 2));
+	}
+
+	public static void main(String[] args) {
+		new Example();
+	}
+
+}
