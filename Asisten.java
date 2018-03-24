eef158ca1e8347bd77eb39162e2f1703b8b947d6
ambarsetiaawan <ambarsetiaawan@gmail.com>
2018-03-24 17:47:25 +0700

    Update Asisten.java

--- Asisten.java
+++ Asisten.java
@@ -1,2 +1,15 @@
 //KELAS ASISTEN
 //Asisten.java
+
+public class Asisten implements Runnable{
+	private String name;
+	
+	private Asisten(String name){
+		this.name = name;
+	}
+	
+	public void run(){
+		System.out.println("Asisten "+name+" sedang bergerak.");
+		//Asisten ngapain
+	}	
+}
