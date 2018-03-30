20dbf7bba2f11ec80ff4fbff4cec87bfa5d4cf21
kennyongkojoyo <37732703+kennyongkojoyo@users.noreply.github.com>
2018-03-30 22:52:47 +0700

    Update PraktikumBuilder.java

--- PraktikumBuilder.java
+++ PraktikumBuilder.java
@@ -1,60 +1,89 @@
 public class PraktikumBuilder{
-	private final Asisten a1,a2;
-	private final Praktikan p1,p2,p3,p4;
-	private final int[] map;
+	private Builder b;
 	
 	public static class Builder{
 		private Asisten a1,a2;
-		priavte Praktikan p1p1,p2,p3,p4;
+		private Praktikan p1,p2,p3,p4;
 		private int[] map;
 		
-		public Builder a1(Asisten a1){
+		public void setA1(Asisten a1){
 			this.a1 = a1;
-			return this;
 		}
 		
-		public Builder a2(Asisten a2){
+		public void setA2(Asisten a2){
 			this.a2 = a2;
-			return this;
 		}
 		
-		public Builder p1(Praktikan p1){
+		public void setP1(Praktikan p1){
 			this.p1 = p1;
-			return this;
 		}
 		
-		public Builder p2(Praktikan p2){
+		public void setP2(Praktikan p2){
 			this.p2 = p2;
-			return this;
 		}
 		
-		public Builder p3(Praktikan p3){
+		public void setP3(Praktikan p3){
 			this.p3 = p3;
-			return this;
+
 		}
 		
-		public Builder p4(Praktikan p4){
+		public void setP4(Praktikan p4){
 			this.p4 = p4;
-			return this;
 		}
 		
-		public Builder map(int[] map){
+		public void setMap(int[] map){
 			this.map = map;
-			return this;
 		}
 		
-		public PraktikumBuilder build(){
-			return new PraktikumBuilder(this);
-		}
 	}
 	
-	private PraktikumBuilder(Builder b){
-		this.a1 = b.a1;
-		this.a2 = b.a2;
-		this.p1 = b.p1;
-		this.p2 = b.p2;
-		this.p3 = b.p3;
-		this.p4 = b.p4;
-		this.map = b.map;
+	public PraktikumBuilder(Builder b){
+		this.b = new Builder ();
+	}
+	
+	public void buildA1 () {
+		Asisten a1 = new Asisten(0,0);
+		b.setA1(a1);
+	}
+	
+	public void buildA2 () {
+		Asisten a2 = new Asisten(4,0);
+		b.setA2(a2);
+	}
+	
+	public void buildP1 () {
+		Praktikan p1 = new Praktikan();
+		b.setP1(p1);
+	}
+	
+	public void buildP2 () {
+		Praktikan p2 = new Praktikan();
+		b.setP2(p2);
+	}
+	
+	public void buildP3 () {
+		Praktikan p3 = new Praktikan();
+		b.setP3(p3);
+	}
+	
+	public void buildP4 () {
+		Praktikan p4 = new Praktikan();
+		b.setP4(p4);
+	}
+	
+	public Builder getBuilder() {
+		return this.b;
+	}
+	
+	public void BuildWorld () {
+		this.buildA1();
+		this.buildA2();
+		this.buildP1();
+		this.buildP2();
+		this.buildP3();
+		this.buildP4();
 	}
 }
+
+
+
