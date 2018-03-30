cea788201c303e9131b097754e93e702457b9f7c
ambarsetiaawan <ambarsetiaawan@gmail.com>
2018-03-30 12:43:35 +0700

    Create MainBaru.java

---
+++ MainBaru.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+	public static SingletonQueue<Praktikan> q = SingletonQueue<Praktikan>.getIns();
+	
+	public static void main(String[] args){
+		Scanner in1 = new Scanner(new File("praktikan.txt")); //posisi praktikan
+		Scanner in2 = new Scanner(new File("asisten.txt")); // posisi asisten
+	}
+}
