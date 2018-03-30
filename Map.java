d7a32becf518e9a253bda8b83d38f46e8c4ec33a
sebastianjer <32337385+sebastianjer@users.noreply.github.com>
2018-03-30 13:34:31 +0700

    Create Map.java

---
+++ Map.java
@@ -0,0 +1,62 @@
+public class Map{
+	private Existence[][] map;
+	private int rows;
+	private int cols;
+	
+	public Map(int rows, int cols, Existence a, Existence border){
+		this.rows = rows;
+		this.cols = cols;
+		map = new Existence[rows][cols];
+		
+		for (int i = 0; i < rows; i++){
+			for (int j = 0; j < cols; j++){
+				if ((i == 0) || (j == 0)){
+					map[i][j] = border;
+				}else{
+					map[i][j] = a;
+				}
+			}
+		}
+	}
+	
+	public Existence[][] getMap(){
+		return map;
+	}
+	
+	public void insert(int x, int y, Existence tag){
+		map[x][y] = tag;
+	}
+	
+	public void moveLeft(int x, int y){
+		Existence temp = map[x][y];
+		map[x][y] = map[x][y-1];
+		map[x][y-1] = temp;
+	}
+	
+	public void moveRight(int x, int y){
+		Existence temp = map[x][y];
+		map[x][y] = map[x][y+1];
+		map[x][y+1] = temp;
+	}
+	
+	public void moveUp(int x, int y){
+		Existence temp = map[x][y];
+		map[x][y] = map[x-1][y];
+		map[x-1][y] = temp;
+	}
+	
+	public void moveDown(int x, int y){
+		Existence temp = map[x][y];
+		map[x][y] = map[x+1][y];
+		map[x+1][y] = temp;
+	}
+	
+	public void printRoom(){
+		for (int i = 0; i < rows; i++){
+			for (int j = 0; j < cols; j++){
+				System.out.println(map[i][j]);
+			}
+			System.out.println("");
+		}
+	}
+}
